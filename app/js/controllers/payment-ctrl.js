998e09451d9b606ebe652dc7921e57f6ccf6c020
sumanta <sumanta90parida@gmail.com>
2016-04-12 20:51:32 +0530

    payment tab active logic done plus css of tab for mobile version corrected

--- app/js/controllers/payment-ctrl.js
+++ app/js/controllers/payment-ctrl.js
@@ -1,6 +1,12 @@
 define(['./index'], function (controllers) {
     'use strict';
     controllers.controller('paymentCtrl', ['$scope', function ($scope) {
- 
+    	 $scope.changeName = null;
+    	      $scope.changeSlide = function(value) {
+    	      	alert(value);
+    	      	$scope.changeName = value;
+    	      	console.log($scope.changeName)
+    	      }
+
     }]);
 });
