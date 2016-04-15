cec7cbb38f7cb65d143ac3f42ceb4ddc1d467392
Siddhesh Rane <siddhesh.rane@craftsvilla.com>
2016-04-15 12:10:18 +0530

    feat(checkout)-checkout page coupon code done

--- app/js/config.js
+++ app/js/config.js
@@ -6,26 +6,22 @@ define(['angular'], function (angular) {
   return angular.module('app.config', [])
       .constant('VERSION', '0.0.1')
       //.constant('HOST', 'http://localhost:8000/api')
-      .constant('HOST', 'https://api.myjson.com/bins/')
-      //.constant('HOST', 'https://www.craftsvilla.com/api')
+      //.constant('HOST', 'https://api.myjson.com/bins/')
+      .constant('HOST', 'http://dev7.craftsvilla.com/')
       .constant('ENVIRONMENT', 'development')
       .config(['$httpProvider', function ($httpProvider) {
-        //delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        //$httpProvider.defaults.headers.post['Accept'] = 'application/json, text/javascript';
-        //$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-        //$httpProvider.defaults.headers.post['Access-Control-Max-Age'] = '1728000';
-        //$httpProvider.defaults.headers.common['Access-Control-Max-Age'] = '1728000';
-        //$httpProvider.defaults.headers.common['Accept'] = 'application/json, text/javascript';
-        //$httpProvider.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
-        //$httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-        //$httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.useXDomain = true;
-
+        // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        // $httpProvider.defaults.headers.post['Accept'] = 'application/json, text/javascript';
+        // // $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        // $httpProvider.defaults.headers.post['Access-Control-Max-Age'] = '1728000';
+        // $httpProvider.defaults.headers.common['Access-Control-Max-Age'] = '1728000';
+        // $httpProvider.defaults.headers.common['Accept'] = 'application/json, text/javascript';
+        // $httpProvider.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
+        // $httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+        // $httpProvider.defaults.useXDomain = true;
         $httpProvider.defaults.headers.common = {};
         $httpProvider.defaults.headers.post = {};
         $httpProvider.defaults.headers.put = {};
         $httpProvider.defaults.headers.patch = {};
-        
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
       }])
 });
