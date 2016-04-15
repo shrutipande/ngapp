f3b25e2f1d6b42d66aee2ab35c03e52c02b1a2d8
Avinash Verma <avinash.verma@craftsvilla.com>
2016-04-15 16:36:16 +0530

    Social integrations

--- app/js/config.js
+++ app/js/config.js
@@ -1,31 +1,43 @@
 /*global define */
 
-define(['angular'], function (angular) {
-  'use strict';
+define(['angular'], function(angular) {
+    'use strict';
 
-  return angular.module('app.config', [])
-      .constant('VERSION', '0.0.1')
-      //.constant('HOST', 'http://localhost:8000/api')
-      .constant('HOST', 'https://api.myjson.com/bins/')
-      //.constant('HOST', 'https://www.craftsvilla.com/api')
-      .constant('ENVIRONMENT', 'development')
-      .config(['$httpProvider', function ($httpProvider) {
-        //delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        //$httpProvider.defaults.headers.post['Accept'] = 'application/json, text/javascript';
-        //$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-        //$httpProvider.defaults.headers.post['Access-Control-Max-Age'] = '1728000';
-        //$httpProvider.defaults.headers.common['Access-Control-Max-Age'] = '1728000';
-        //$httpProvider.defaults.headers.common['Accept'] = 'application/json, text/javascript';
-        //$httpProvider.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
-        //$httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-        //$httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.useXDomain = true;
+    return angular.module('app.config', ['satellizer'])
+        .constant('VERSION', '0.0.1')
+        //.constant('HOST', 'http://localhost:8000/api')
+        .constant('HOST', 'https://api.myjson.com/bins/')
+        //.constant('HOST', 'https://www.craftsvilla.com/api')
+        .constant('ENVIRONMENT', 'development')
+        .config(['$httpProvider', function($httpProvider) {
+            //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            //$httpProvider.defaults.headers.post['Accept'] = 'application/json, text/javascript';
+            //$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+            //$httpProvider.defaults.headers.post['Access-Control-Max-Age'] = '1728000';
+            //$httpProvider.defaults.headers.common['Access-Control-Max-Age'] = '1728000';
+            //$httpProvider.defaults.headers.common['Accept'] = 'application/json, text/javascript';
+            //$httpProvider.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
+            //$httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+            //$httpProvider.defaults.useXDomain = true;
+            $httpProvider.defaults.useXDomain = true;
 
-        $httpProvider.defaults.headers.common = {};
-        $httpProvider.defaults.headers.post = {};
-        $httpProvider.defaults.headers.put = {};
-        $httpProvider.defaults.headers.patch = {};
-        
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-      }])
+            $httpProvider.defaults.headers.common = {};
+            $httpProvider.defaults.headers.post = {};
+            $httpProvider.defaults.headers.put = {};
+            $httpProvider.defaults.headers.patch = {};
+
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        }])
+        .config([ '$authProvider', function($authProvider) {
+
+            $authProvider.facebook({
+                clientId: '263834947290125',
+                responseType: 'token'
+            });
+
+            $authProvider.google({
+                clientId: 'Google Client ID'
+            });
+
+        }]);
 });
--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,8 @@ require.config({
         'angular': '../bower_components/angular/angular.min',
         'angular-sanitize' : '../bower_components/angular-sanitize/angular-sanitize.min',
         'angular-ui-router' : '../bower_components/angular-ui-router/release/angular-ui-router.min',
-        'angular-credit-cards' : '../bower_components/angular-credit-cards/release/angular-credit-cards'
+        'angular-credit-cards' : '../bower_components/angular-credit-cards/release/angular-credit-cards',
+        'satellizer' : '../bower_components/satellizer/satellizer'
     },
 
     shim: {
@@ -22,6 +23,9 @@ require.config({
         },
         'angular-ui-router': {
             deps: ['angular']
+        },
+        'satellizer': {
+            dep: ['angular']
         }
     }
 });
