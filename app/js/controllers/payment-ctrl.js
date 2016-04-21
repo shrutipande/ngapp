b172d66067bb71ee97bf0d696a4d8454e5c55d07
Avinash Verma <avinash.verma@craftsvilla.com>
2016-04-21 10:49:47 +0530

    Credit / Debit form validation from function instead of inline code

--- app/js/controllers/payment-ctrl.js
+++ app/js/controllers/payment-ctrl.js
@@ -257,7 +257,7 @@ define(['./index'], function (controllers) {
 							var check1 = form.cardM.$dirty && form.cardY.$dirty;
 							var check2 = form.cardM.$valid;
 							var check3 = form.cardY.$valid;
-							var check4 = form.cardY.$xblur && form.cardM.$xblur;
+							var check4 = true;//form.cardY.$xblur && form.cardM.$xblur;
 							var check5 = new Date() > new Date(form.cardY.$modelValue, form.cardM.$modelValue - 1);
 
 							if(check1) {
@@ -271,10 +271,12 @@ define(['./index'], function (controllers) {
 							}
 							else return false;
 					}
-
-						(!forms.creditForm.cardM.$valid && !forms.creditForm.cardM.$pristine && forms.creditForm.cardM.$xblur) || (!forms.creditForm.cardY.$valid && !forms.creditForm.cardY.$pristine && forms.creditForm.cardM.$xblur)
-					default:
-
+				case 'cardCVC':
+					return form[element].$invalid && !form[element].$pristine && form[element].$xblur
+				case 'payment':
+					return form.$invalid || $scope.validate('cardExp', form);
+				default:
+					return false;
 				}
 			}
 
