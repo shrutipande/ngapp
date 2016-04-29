a84e82506ce8172c8f679d33fb19e2cbc8841077
Avinash Verma <avinash.verma@craftsvilla.com>
2016-04-29 17:35:18 +0530

    hashbang removed

--- app/js/routes.js
+++ app/js/routes.js
@@ -5,58 +5,58 @@ define(['./app'], function (app) {
   'use strict';
   app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $urlRouterProvider) {
 
-    $locationProvider.html5Mode(false);
+    $locationProvider.html5Mode(true);
 
     $stateProvider
         .state('login', {
-          url: "/login",
+          url: "/buy/login",
           templateUrl: 'partials/login.html',
           controller: 'loginCtrl',
           title: 'Login',
           authenticate: false
         })
         .state('shipping', {
-          url: "/shipping",
+          url: "/buy/shipping",
           templateUrl: 'partials/shipping.html',
           controller: 'shippingCtrl',
           title: 'Shipping',
           authenticate: true
         })
         .state('payment', {
-          url: "/payment/:platform/:quoteId",
+          url: "/buy/payment/:platform/:quoteId",
           templateUrl: 'partials/payment.html',
           controller: 'paymentCtrl',
           title: 'Payment',
           authenticate: false
         })
         .state('payments-error', {
-          url: "/payment-error",
+          url: "/buy/payment-error",
           templateUrl: 'partials/payment-error.html',
           controller: 'paymentErrorCtrl',
           title: 'Error',
           authenticate: false
         })
         .state('payments-success', {
-          url: "/payment-success",
+          url: "/buy/payment-success",
           templateUrl: 'partials/payment-success.html',
           controller: 'paymentSuccessCtrl',
           title: 'Success',
           authenticate: false
         })
         .state('cart', {
-          url: "/cart",
+          url: "/buy/cart",
           templateUrl: 'partials/checkout-cart.html',
           controller: 'checkoutCartCtrl',
           title: 'Shopping Cart',
           authenticate: false
         })
         .state('empty-cart', {
-          url: "/empty-cart",
+          url: "/buy/empty-cart",
           templateUrl: 'partials/empty-cart.html',
           controller: 'emptyCartCtrl',
           title: 'Empty cart',
           authenticate: false
         });
-    $urlRouterProvider.otherwise("/cart");
+    $urlRouterProvider.otherwise("/buy/cart");
   }]);
 });
