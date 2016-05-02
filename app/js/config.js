cecff49fa62b671f7ceafeff89f954e629af98ee
Avinash Verma <avinash.verma@craftsvilla.com>
2016-05-02 17:18:37 +0530

    changes

--- app/js/config.js
+++ app/js/config.js
@@ -4,7 +4,7 @@ define(['angular'], function(angular) {
      return angular.module('app.config', ['satellizer','ngAnimate'])
         .constant('VERSION', '0.0.1')
      //    .constant('PRODUCTURL', 'http://dev7.craftsvilla.com/catalog/product/view/id/')
-        .constant('PRODUCTURL', 'https://secure.craftsvilla.com/catalog/product/view/id/')
+        .constant('PRODUCTURL', 'https://www.craftsvilla.com/catalog/product/view/id/')
      //    .constant('HOST', 'http://dev7.craftsvilla.com/')
         .constant('HOST', 'https://securestatic.craftsvilla.com/')
 
@@ -39,21 +39,21 @@ define(['angular'], function(angular) {
             delete $httpProvider.defaults.headers.common['X-Requested-With'];
         }])
         .config(['$authProvider', 'HOST', function($authProvider, HOST) {
-            $authProvider.baseUrl = 'https://secure.craftsvilla.com';
+            $authProvider.baseUrl = 'https://securestatic.craftsvilla.com';
           //   $authProvider.baseUrl = 'http://dev7.craftsvilla.com';
             $authProvider.withCredentials = true;
 
             // $authProvider.httpInterceptor = function() { return true; },
 
             $authProvider.facebook({
-                clientId: '1696658660596971',
+                clientId: '1668947976707075',
                 responseType: 'token',
                 url: '/checkoutService/index/socialLogin',
                 skipAuthorization: true,
             });
 
             $authProvider.google({
-                clientId: '34235241642-4rsq630ova71avmsph5ded5uvp3tac8t.apps.googleusercontent.com',
+                clientId: '284180597119-51glidbrmd6u9lg77hjvqhbq69vl62j9.apps.googleusercontent.com',
                 url: '/checkoutService/index/socialLogin',
                 skipAuthorization: true,
             });
