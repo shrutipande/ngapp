6f0ef7afc9043ad405126ddec6702599c7bf51f3
Avinash Verma <avinash.verma@craftsvilla.com>
2016-05-03 12:09:03 +0530

    changes

--- app/js/controllers/login-ctrl.js
+++ app/js/controllers/login-ctrl.js
@@ -236,6 +236,38 @@ define(['./index'], function (controllers) {
       		$scope.loginTracker();
 
     };
+
+		$scope.IE = (function () {
+	    "use strict";
+
+	    var ret, isTheBrowser,
+	        actualVersion,
+	        jscriptMap, jscriptVersion;
+
+	    isTheBrowser = false;
+	    jscriptMap = {
+	        "5.5": "5.5",
+	        "5.6": "6",
+	        "5.7": "7",
+	        "5.8": "8",
+	        "9": "9",
+	        "10": "10",
+					"11": "11"
+	    };
+	    jscriptVersion = new Function("/*@cc_on return @_jscript_version; @*/")();
+
+	    if (jscriptVersion !== undefined) {
+	        isTheBrowser = true;
+	        actualVersion = jscriptMap[jscriptVersion];
+	    }
+
+	    ret = {
+	        isTheBrowser: isTheBrowser,
+	        actualVersion: actualVersion
+	    };
+
+	    return ret;
+	}());
 	$scope.initLogin();
 
 	}]);
